0a39fb8822c4c65184440f9d4b23c205f4ef7ad8
Marcus Sacramento <marcus.sacramento.millenium@caixaseguradora.com.br>
2019-01-30 16:01:22 -0200

    Atualização para submissão baseada em propriedades

---
+++ package/rtc_controller/rtc_controller.py
@@ -0,0 +1,100 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import re
+
+import subprocess
+import shlex
+import configparser
+
+
+
+config = configparser.ConfigParser()
+config.read('arquivos/properties')
+
+jazz_url = config.get('JAZZ','url')
+user = config.get('JAZZ','user')
+password = config.get('JAZZ','password')
+
+
+class Changeset(object):
+    def __init__(self, change_id,autor,comentario,data):
+        self.change_id=change_id
+        self.autor=autor
+        self.comentario=comentario
+        self.data=data
+
+##class Changes(object):
+##    def __init__(self,alteracao)
+
+
+
+def carrega_workspace(workspace,diretorio_local):
+    if not os.path.exists(diretorio_local):
+        os.makedirs(diretorio_local)
+    cmd='lscm load --all '+workspace+' -d '+diretorio_local+' --allow -u '+user+' -P '+password+' -r '+jazz_url
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    print(out)
+    return out
+
+def descarrega_workspace(workspace,diretorio_local):
+    cmd='lscm unload --all '+workspace+' -d '+diretorio_local+' -u '+user+' -P '+password+' -r '+jazz_url
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    return out
+
+def lista_changes(workspace,diretorio_local,change):
+    cmd='lscm ls changes -w '+workspace+' '+change+' -u '+user+' -P '+password+' -r '+jazz_url
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    for linha in str(out).split('\\n'):
+        change_id=''
+        changes=re.search(r'[-]{3}[a-z][-][ ][(]([0-9]{4,5})[)][ ](.*)',linha)
+        if(changes):
+            change_id=changes.group(1)
+            autor=changes.group(2)
+            comentario=changes.group(3)
+            data=changes.group(4)
+            changes_result.append(Changeset(change_id,autor,comentario,data))
+    for i in changes_result:
+        print(i.data)
+    return changes_result
+
+
+
+    return out
+
+def lista_changesets(diretorio):
+    cmd='lscm show status -d '+diretorio
+    changes_result = []
+
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    for linha in str(out).split('\\n'):
+        changes=re.search(r'[(]([0-9]{4,5})[)][ ]{1,}[-]{1,}[$][ ](.*)[ ][""].*["<](.*)[>"].*([0-9]{2}[-][a-z]{3}[-][0-9]{4})',linha)
+        if(changes):
+            change_id=changes.group(1)
+            autor=changes.group(2)
+            comentario=changes.group(3)
+            data=changes.group(4)
+            changeset=Changeset(change_id,autor,comentario,data)
+            changes_result.append(changeset)
+            for i in changes_result:
+                print(i.data)
+    return changes_result
+
+def aceita_changeset(workspace,changeset):
+    cmd='lscm accept '+changeset+' -t '+workspace+' -u '+user+' -P '+password+' -r '+jazz_url
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    return out
+
+def entrega_changeset(workspace_origem,workspace_destino,changeset):
+    cmd='lscm deliver '+changeset+' -s '+workspace_origem+' -t '+workspace_destino+' -u '+user+' -P '+password+' -r '+jazz_url
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    return out
+
+
--- submissao.py
+++ submissao.py
@@ -2,40 +2,115 @@
 import re
 import datetime
 import configparser
+import sys
 
 from package.util.util import *
+from package.rtc_controller.rtc_controller import *
+
 
 config = configparser.ConfigParser()
 config.read('arquivos/properties')
 
-hostname = config.get('zos','host')
-port = int(config.get('zos','port'))
-ftp_user = config.get('zos.ftp','user')
-ftp_pass = config.get('zos.ftp','password')
-job_modelo='arquivos/cartao/SMART/dev/JDSZCOMP_I.model'
-job_final='arquivos/cartao/SMART/dev/JDSZCOMP'
+sistema=sys.argv[0]
+ambiente=sys.argv[1]
+diretorio_fonte=config.get('JAZZ','fonte')
+workspace=config.get('JAZZ','workspace')
+
+
+hostname = config.get('ZOS.'+ambiente,'host')
+port = int(config.get('ZOS.'+ambiente,'port'))
+ftp_user = config.get('ZOS.FTP.'+ambiente,'user')
+ftp_pass = config.get('ZOS.FTP.'+ambiente,'password')
 
 arquivolog = 'arquivos/log/'+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")+'.log'
+id_executor = 'MS'+datetime.datetime.now().strftime("%y%m%d")
+lib_batch=config.get(sistema+'.'+ambiente,'lib_batch')
+lib_booklib=config.get(sistema+'.'+ambiente,'lib_booklib')
+lib_cics=config.get(sistema+'.'+ambiente,'lib_cics')
+
+saida = open(arquivolog,'w')
+saida.write('######################  Carregamento de fontes ######################\n')
+saida.write(carrega_workspace(workspace,diretorio_fonte).decode('latin-1')+'\n')
+
+saida.write('######################  Upload de código fonte ######################\n')
+with open('arquivos/PROGRAMAS', 'rt') as f:
+    result=''
+    for linha in f.read().split('\n'):
+        if len(linha)==0:
+            continue
+        compilacao = linha.split(';')
+        programa = compilacao[0]
+        classe  = compilacao[1]
+        try:
+            if(classe=='BATCH'):
+                job=open(diretorio_fonte+'/COBOL.GCS/BATCH/'+programa+'.cbl','rb')
+                ftp = ftplib.FTP()
+                ftp.connect(hostname, port)
+                ftp.set_debuglevel(0)
+                ftp.login(ftp_user, ftp_pass)
+                ftp.voidcmd('site filetype=seq')
+                ftp.pwd()
+                ftp.cwd('..')
+                ftp.cwd(lib_batch)
+                result = ftp.storlines('STOR '+programa,job)
+                ftp.close()
+            if(classe=='CICS'):
+                job=open(diretorio_fonte+'/COBOL.GCS/BATCH/'+programa+'.cbl','rb')
+                ftp = ftplib.FTP()
+                ftp.connect(hostname, port)
+                ftp.set_debuglevel(0)
+                ftp.login(ftp_user, ftp_pass)
+                ftp.voidcmd('site filetype=seq')
+                ftp.pwd()
+                ftp.cwd('..')
+                ftp.cwd(lib_cics)
+                result = ftp.storlines('STOR '+programa,job)
+                ftp.close()
+            if(classe=='BOOKLIB'):
+                job=open(diretorio_fonte+'/COBOL.GCS/BOOKLIB/'+programa+'.bkl','rb')
+                ftp = ftplib.FTP()
+                ftp.connect(hostname, port)
+                ftp.set_debuglevel(0)
+                ftp.login(ftp_user, ftp_pass)
+                ftp.voidcmd('site filetype=seq')
+                ftp.pwd()
+                ftp.cwd('..')
+                ftp.cwd(lib_booklib)
+                result = ftp.storlines('STOR '+programa,job)
+                ftp.close()
+            saida.write("{};{}\n".format(programa,result))
+        except:
+            saida.write("{};{}\n".format(programa,sys.exc_info()[1]))
+        f.close()
 
 ftp = ftplib.FTP()
 ftp.connect(hostname, port)
 ftp.set_debuglevel(0)
 ftp.login(ftp_user, ftp_pass)
 ftp.voidcmd('site filetype=jes')
-ftp.pwd()
-saida = open(arquivolog,'w')
+
+saida.write('######################     SUBMISSÃO DE JOBS   ######################\n')
 with open('arquivos/PROGRAMAS', 'rt') as f:
-    for programa in f.read().split('\n'):
-        if len(programa)==0:
+    for linha in f.read().split('\n'):
+        if len(linha)==0:
             continue
-        sub_string_file(job_modelo,'arquivos/cartao/SMART/dev/JDSZCOMP_I.tmp','ID_PROGRAMA',programa)
-        sub_string_file('arquivos/cartao/SMART/dev/JDSZCOMP_I.tmp',job_final,'WLM_PROGRAMA','DES_SP'+programa[:4]+'01')
-        job=open(job_final,'rb')
-        result = ftp.storlines('STOR JDSZCOMP',job)
-        job_number= re.search(r'(JOB[0-9]{5})',result)
-        if job_number:
-            saida.write("{};{}\n".format(programa,job_number.group(1)))
-        else:
-            saida.write("{};{}\n".format(programa,'Erro submissão'))
+        compilacao = linha.split(';')
+        programa = compilacao[0]
+        classe  = compilacao[1]
+        if(classe=='BATCH'):
+            tipo = compilacao[2]
+            job_modelo='arquivos/cartao/'+sistema+'/'+ambiente+'/'+tipo+'.model'
+            job_final='arquivos/cartao/'+sistema+'/JOB'+sistema+tipo
+            sub_string_file(job_modelo,'arquivos/cartao/'+sistema+'/'+ambiente+'/JOBJCL.tmp','ID_PROGRAMA',programa)
+            sub_string_file('arquivos/cartao/'+sistema+'/'+ambiente+'/JOBJCL.tmp','arquivos/cartao/'+sistema+'/'+ambiente+'/JOBJCLI.tmp','ID_EXECUTOR',id_executor)
+            sub_string_file('arquivos/cartao/'+sistema+'/'+ambiente+'/JOBJCLI.tmp',job_final,'WLM_PROGRAMA','DES_SP'+programa[:4]+'01')
+            job=open(job_final,'rb')
+            result = ftp.storlines('STOR JOB'+sistema,job)
+            job_number= re.search(r'(JOB[0-9]{5})',result)
+            if job_number:
+                saida.write("{};{}\n".format(programa,job_number.group(1)))
+            else:
+                saida.write("{};{}\n".format(programa,'Erro submissão'))
+saida.write('###################### Encerramento compilação  ######################\n')
 saida.close()
 ftp.quit()
